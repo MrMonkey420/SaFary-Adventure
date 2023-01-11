a4c1b8c87f9d7931e6343775a171d27dd13bdb47
Marc Escandell <marc.escandell.alonso@estudiantat.upc.edu>
2023-01-11 13:31:30 +0100

    delokos

---
+++ Game/BalasAgua.cpp
@@ -0,0 +1,2 @@
+#include "BalasAgua.h"
+
---
+++ Game/BalasAgua.h
@@ -0,0 +1,15 @@
+#ifndef _BALASAGUA_H_
+#define _BALASAGUA_H_
+
+#include "Entity.h"
+
+class BalasAgua : public Entity {
+private:
+
+public:
+
+
+
+}; 
+
+#endif // !_BALASAGUA_H_
